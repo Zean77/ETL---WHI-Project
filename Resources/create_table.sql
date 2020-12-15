921b24771f2d54b29ec0263f188be4ce95896385
Robert Englund <robertenglund980@gmail.com>
2020-12-14 20:16:34 -0600

    updated schema and initial code for happiness and load

--- Resources/create_table.sql
+++ Resources/create_table.sql
@@ -2,19 +2,16 @@
 CREATE TABLE country_stats (
   stats_year VARCHAR(4) NOT NULL,
   stats_country VARCHAR(30) NOT NULL,
-  hap_score decimal
-  hap_GDP decimal
-  hap_social decimal
-  hap_health decimal
-  hap_freedom decimal
-  hap_generosity decimal
-  hap_corruption decimal
-  hap_residual decimal
-  WDI_income_per_capita decimal 
-  WDI_social_protection decimal
-  WDI_fieldx decimal
-  WDI_fieldy decimal
-  WDI_fieldz decimal
-  xxx_life_expectancy decimal
-  xxx_divorce_rate decimal
+  hap_score decimal,
+  hap_GDP decimal,
+  hap_social decimal,
+  hap_health decimal,
+  hap_freedom decimal,
+  hap_generosity decimal,
+  hap_corruption decimal,
+  hap_residual decimal,
+  WDI_income_per_capita decimal, 
+  WDI_military_exp decimal,
+  WBK_life_expectancy decimal,
+  WPR_crime_rate decimal
 );
